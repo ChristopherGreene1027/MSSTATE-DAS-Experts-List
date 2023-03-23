a52ac28dd6bb1d37124cc6c5f633182514b81e3c
Grace Ball <gb968@msstate.edu>
2023-03-23 16:34:33 -0500

    Fix images

--- Django/experts/urls.py
+++ Django/experts/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -9,3 +12,5 @@ urlpatterns = [
     path('expert/<int:expert_id>',views.expert,name='expert'),
     path('topic/<int:topic_id>',views.topic,name='topic'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
