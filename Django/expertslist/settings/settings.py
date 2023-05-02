de35b532c12178293c50cf24a541d7ae213ee3f0
Chris Greene <55257067+ChristopherGreene1027@users.noreply.github.com>
2023-05-02 18:13:11 -0500

    another commit

--- Django/expertslist/settings/settings.py
+++ Django/expertslist/settings/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 # deleted development settings
 
@@ -113,6 +113,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
